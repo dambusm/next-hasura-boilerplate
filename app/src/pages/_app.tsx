62dc61b12a6c13d03a33a17d1df0aa2cffae0290
Will Schaller <willschaller@gmail.com>
2020-12-27 16:38:02 +0100

    Add next-auth provider

---
+++ app/src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import { Provider } from 'next-auth/client';
+import { AppProps } from 'next/app';
+import React from 'react';
+
+export default function App({ Component, pageProps }: AppProps) {
+    return (
+        <Provider session={pageProps.session}>
+            <Component {...pageProps} />
+        </Provider>
+    );
+}
