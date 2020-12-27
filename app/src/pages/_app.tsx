a91ad46066a5f330371a977176cb69d8dc737fba
Will Schaller <willschaller@gmail.com>
2020-12-27 20:12:01 +0100

    Add water.css

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { Provider } from 'next-auth/client';
 import { AppProps } from 'next/app';
 import React from 'react';
+import 'water.css/dist/light.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
