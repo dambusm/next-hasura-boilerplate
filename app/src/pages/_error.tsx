e4a6471e75a64c6b6ef3eec6de4bf0a011a987cc
Will Schaller <willschaller@gmail.com>
2021-07-29 17:38:43 +0200

    Get error code on error page

--- app/src/pages/_error.tsx
+++ app/src/pages/_error.tsx
@@ -1,6 +1,10 @@
-import { ErrorProps } from 'next/dist/pages/_error';
+import { NextPage, NextPageContext } from 'next';
+import { logger } from './_app';
 
-export default function Error(props: ErrorProps) {
+const Error: NextPage<Props> = (props) => {
+  const { statusCode } = props;
+  const isServerSideError = !!statusCode; // 500 errors are handled both client-side and server-side by the Error component.
+
   return (
     <div
       style={{
@@ -15,4 +19,15 @@ export default function Error(props: ErrorProps) {
       </p>
     </div>
   );
-}
+};
+
+const getInitialProps = ({ res, err }: NextPageContext) => {
+  logger.error(err, res);
+  return { statusCode: res?.statusCode || err?.statusCode };
+};
+
+Error.getInitialProps = getInitialProps;
+
+type Props = ReturnType<typeof getInitialProps>;
+
+export default Error;
